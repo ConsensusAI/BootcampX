bb0df2f9436096dada323601c80dce28d90dace8
ConsensusAI <malik.iqbal@hotmail.ca>
2022-04-21 00:23:31 +0000

    Refactor with parameterized query to prevent sql injection vulnerabilities

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 const args = process.argv.slice(2);
 
 let cohort = args[0];
-let limit = args[1];
+let limit = args[1] || 5;
 
 const pool = new Pool({
   user: "vagrant",
@@ -11,15 +11,15 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+const query = `SELECT students.id, students.name AS name, cohorts.name AS cohort_name 
+FROM students 
+JOIN cohorts ON cohorts.id = cohort_id 
+WHERE cohorts.name LIKE $1 LIMIT $2;`;
+
+const values = [`%${cohort}%`, limit];
+
 pool
-  .query(
-    `
-SELECT students.id, students.name AS name, cohorts.name AS cohort_name
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit};`
-  )
+  .query(query, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,16 +10,18 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+const query = `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
-ORDER BY teachers.name; `
-  )
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;`;
+
+const values = [`%${cohort}%`];
+
+pool
+  .query(query, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(`${user.cohort}: ${user.teacher}`);
